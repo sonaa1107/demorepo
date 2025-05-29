d38fbbae1ec0faf842331dac918eac16a6029f7f
sonaa1107 <sonaagrawal1107@gmail.com>
2025-05-29 13:13:45 +0530

    add edit and remive button

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ const addTodo=()=>{
     if(inputText.length<=0){
        return alert("add a todo")
     }
+
     const li=document.createElement('li')
     const p=document.createElement('p')
     p.innerHTML=inputText
     li.appendChild(p)
+
+    const editbtn=document.createElement('button')
+    editbtn.innerText='Edit'
+    editbtn.classList.add("btn")
+    li.appendChild(editbtn)
+
+    const deletebtn=document.createElement('button')
+    deletebtn.innerText='Remove'
+    deletebtn.classList.add("btn")
+    li.appendChild(deletebtn)
+
     todo.appendChild(li)
     inputBox.value=""
 
 
 }
+const updateToDo=(e)=>{
+    if(e.target.innerHTML==='Remove'){
+        todo.removeChild(e.target.parentElement)
+    }
+    if(e.target.innerHTML==='Edit'){
+        
+    }
+}
+
 addbtn.addEventListener('click',addTodo);
